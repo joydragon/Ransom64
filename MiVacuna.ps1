8bd65654359822430c4bdcfa20e6155e4ab07732
Joy Dragon <joydragon@gmail.com>
2024-02-23 11:03:06 -0300

    Cambio en variable $ext por conflicto
    Cambio en variable $ext por conflicto, se cambia por $extension

--- MiVacuna.ps1
+++ MiVacuna.ps1
@@ -1,8 +1,8 @@
-﻿$ext = @(".palquelee");
+﻿$extension = @(".palquelee");
 Get-ChildItem -Path (Get-ChildItem -Path Env:\USERPROFILE).value | ForEach-Object {
     if($_.Name -eq "Downloads" -or $_.Name -eq "Desktop" -or $_.Name -eq "Documents" -or $_.Name -eq "Videos"){
         Get-ChildItem -Recurse -Path $_.FullName | ForEach-Object {
-            if($ext.Contains($_.Extension)){
+            if($extension.Contains($_.Extension)){
                 $name = $_.BaseName;
                 if($name.Length % 4 -eq 2){$name+="=="}
                 elseif($name.Length % 4 -eq 3){$name+="="}
